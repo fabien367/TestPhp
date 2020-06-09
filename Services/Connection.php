e45603551c64fd1220c9ae53672b8057b267d38e
Fabien Richard <fabien.richard@ausy.lu>
2020-06-09 14:06:01 +0200

    Ajout un nouveau commentaire

--- Services/Connection.php
+++ Services/Connection.php
@@ -3,7 +3,8 @@ class ConnectionBase
 {
     private $bdd;
     public function __construct() 
-    { //Coucou Commentaire
+    { //Coucou Commentaire 
+        // Nouveau commentaire
          $this->bdd = new PDO('mysql:host=localhost;dbname=concert', 'root', '');
     }
   
